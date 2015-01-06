8e12bc61407be34a1125232c6e98d65435e6d8d5
Rodrigo Freitas <rodrigo@idei4.com.br>
2015-01-06 19:28:46 -0200

    Controlando as sessões

---
+++ happyNewYear/controllers/contatos.js
@@ -0,0 +1,10 @@
+module.exports = function(app){
+	var ContatosController = {
+		index: function(req, res){
+			var usuario = req.session.usuario
+			, params = {usuario: usuario};
+			res.render('contatos/index', params);
+		}, 
+	};
+	return ContatosController;
+};
